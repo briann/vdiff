8979024d1680f29a6f9e2f4587062985bbf5519e
Brian Ngo <brian@brianngo.net>
2014-12-24 12:56:09 +0800

    vdiff is default user for DBs, remove height for agents.

--- server/models/agent.js
+++ server/models/agent.js
@@ -3,8 +3,7 @@ var Sequelize = require("sequelize");
 module.exports = function(sequelize, DataTypes) {
   var Agent = sequelize.define('Agent', {
     userAgent: { type: Sequelize.STRING, allowNull: false },
-    width:     { type: Sequelize.INTEGER, allowNull: false },
-    height:    { type: Sequelize.INTEGER, allowNull: false }
+    width:     { type: Sequelize.INTEGER, allowNull: false }
   }, {
     classMethods: {
       associate: function(models) {
